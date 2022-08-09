86dcecb7d6037f5c0d02bf6c247dea739e917a5d
Igor <serchenya1999@mail.ru>
2022-08-09 11:43:10 +0300

    class parallax

--- training.js
+++ training.js
@@ -1,53 +1,50 @@
 const cards = document.querySelectorAll(".parallax-cards-item");
 
-cards.forEach((el) => {
-  const image = el.querySelector(".parallax-cards-item-image");
-  const imageBack = el.querySelector(".parallax-cards-item-image-back");
+class Card {
+  constructor(object) {
+    this.item = object.item;
+    this.image = object.image;
+    this.imageBack = object.imageBack;
+    this.title = object.title;
+    this.titleBack = object.titleBack;
+    this.cardBack = object.cardBack;
+  }
 
-  const title = el.querySelector(".parallax-cards-item-title");
-  const titleBack = el.querySelector(".parallax-cards-item-title-back");
+  transformPanel(e) {
+    let mouseX, mouseY;
+    mouseX = e.layerX;
+    mouseY = e.layerY;
 
-  const cardBack = el.querySelector(".parallax-cards-item-card-back");
+    console.log(this.childNodes[3].childNodes[1].style.transform);
 
-  el.onmousemove = transformPanel;
-  // el.onmouseleave = handleMouseLeave;
+    const centerX = this.clientWidth / 2;
+    const centerY = this.clientHeight / 2;
+    const percentX = ((mouseX - centerX) * 5) / (this.clientWidth / 8);
+    const percentY = -(((mouseY - centerY) * 5) / (this.clientHeight / 8));
 
-  let mouseX, mouseY;
+    if (this.classList.contains("parallax-cards-item-rotate")) {
+      // this.childNodes[1].childNodes[1].style.transform =
+      //   "perspective(800px) rotateY(" +
+      //   percentX +
+      //   "deg) rotateX(" +
+      //   (percentY + 180) +
+      //   "deg)";
 
-  const timeout = 160;
-
-  function transformPanel(mouseEvent) {
-    mouseX = mouseEvent.layerX;
-    mouseY = mouseEvent.layerY;
-
-    const centerX = el.clientWidth / 2;
-    const centerY = el.clientHeight / 2;
-    const percentX = ((mouseX - centerX) * 5) / (el.clientWidth / 8);
-    const percentY = -(((mouseY - centerY) * 5) / (el.clientHeight / 8));
-
-    if (el.classList.contains("parallax-cards-item-rotate")) {
-      imageBack.style.transform =
-        "perspective(800px) rotateY(" +
-        percentX +
-        "deg) rotateX(" +
-        (percentY + 180) +
-        "deg)";
-
-      el.style.transform =
+      this.style.transform =
         "perspective(800px) rotateY(" +
         percentX +
         "deg) rotateX(" +
         (percentY + 180) +
         "deg)";
     } else {
-      image.style.transform =
-        "perspective(800px) rotateY(" +
-        percentX +
-        "deg) rotateX(" +
-        percentY +
-        "deg)";
+      // this.childNodes[3].childNodes[1].style.transform =
+      //   "perspective(800px) rotateY(" +
+      //   percentX +
+      //   "deg) rotateX(" +
+      //   percentY +
+      //   "deg)";
 
-      el.style.transform =
+      this.style.transform =
         "perspective(800px) rotateY(" +
         percentX +
         "deg) rotateX(" +
@@ -56,58 +53,73 @@ cards.forEach((el) => {
     }
   }
 
-  function flipCard() {
+  flipCard() {
+    const timeout = 160;
     setTimeout(() => {
-      cardBack.classList.toggle("parallax-cards-item-card-back");
-      titleBack.classList.toggle("parallax-cards-item-title-back");
+      this.cardBack.classList.toggle("parallax-cards-item-card-back");
+      this.titleBack.classList.toggle("parallax-cards-item-title-back");
     }, timeout);
-    el.classList.toggle("parallax-cards-item-rotate");
+    this.item.classList.toggle("parallax-cards-item-rotate");
 
-    if (el.classList.contains("parallax-cards-item-rotate")) {
+    if (this.item.classList.contains("parallax-cards-item-rotate")) {
       setTimeout(() => {
-        title.style.opacity = "0";
-        titleBack.style.transform = "rotateX(180deg)";
-        titleBack.style.top = "45px";
+        this.title.style.opacity = "0";
+        this.titleBack.style.transform = "rotateX(180deg)";
+        this.titleBack.style.top = "45px";
       }, timeout);
 
       setTimeout(() => {
-        el.style.pointerEvents = "auto";
-        el.style.transition = "transform 0s";
+        this.item.style.pointerEvents = "auto";
+        this.item.style.transition = "transform 0s";
       }, 500);
 
-      el.style.transform = "rotateX(180deg)";
-      el.style.transition = "transform 0.5s";
-      el.style.pointerEvents = "none";
+      this.item.style.transform = "rotateX(180deg)";
+      this.item.style.transition = "transform 0.5s";
+      this.item.style.pointerEvents = "none";
     } else {
       setTimeout(() => {
-        title.style.opacity = "1";
-        titleBack.style.transform = "rotateX(180deg)";
-        titleBack.style.top = "45px";
+        this.title.style.opacity = "1";
+        this.titleBack.style.transform = "rotateX(180deg)";
+        this.titleBack.style.top = "45px";
       }, timeout);
 
       setTimeout(() => {
-        el.style.pointerEvents = "auto";
-        el.style.transition = "transform 0s";
+        this.item.style.pointerEvents = "auto";
+        this.item.style.transition = "transform 0s";
       }, 500);
 
-      el.style.transform = "rotateX(0deg)";
-      el.style.transition = "transform 0.5s";
-      el.style.pointerEvents = "none";
+      this.item.style.transform = "rotateX(0deg)";
+      this.item.style.transition = "transform 0.5s";
+      this.item.style.pointerEvents = "none";
     }
   }
 
-  el.onclick = () => flipCard();
+  handleMouseLeave() {
+    console.log(this);
+    setTimeout(() => {
+      this.item.style.transition = "transform 0.3s";
+      this.item.style.transform = "translate(0px,0px)";
+    }, 1500);
 
-  // function handleMouseLeave() {
-  //   setTimeout(() => {
-  //     el.style.transition = "transform 0.3s";
-  //     el.style.transform = "translate(0px,0px)";
-  //   }, 1500);
+    setTimeout(() => {
+      this.item.style.transition = "transform 0s";
+    }, 1600);
 
-  //   setTimeout(() => {
-  //     el.style.transition = "transform 0s";
-  //   }, 1600);
+    this.item.style.transition = "transform 0s";
+  }
+}
 
-  //   el.style.transition = "transform 0s";
-  // }
+cards.forEach((el) => {
+  const card = new Card({
+    item: el,
+    image: el.querySelector(".parallax-cards-item-image"),
+    imageBack: el.querySelector(".parallax-cards-item-image-back"),
+    title: el.querySelector(".parallax-cards-item-title"),
+    titleBack: el.querySelector(".parallax-cards-item-title-back"),
+    cardBack: el.querySelector(".parallax-cards-item-card-back"),
+  });
+
+  el.onclick = () => card.flipCard();
+  el.onmousemove = card.transformPanel;
+  el.onmouseleave = card.handleMouseLeave();
 });
